877b35600184be1437817e6c76dbac246203f9e9
inbarasan <inbarasaninba8@gmail.com>
2023-01-06 17:51:55 +0530

    Added 1833rd Problem

--- maxIcecreamBars/solution.java
+++ maxIcecreamBars/solution.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 class solution{
 
     static int maxIceCream(int[] costs, int coins) {
-        int result = 0, sumCoins = 0;
+        int result = 0, sumCosts = 0;
 
         Arrays.sort(costs);
 
         for(int i=0;i<costs.length;i++){
-            if(sumCoins+costs[i]>coins) break;
-            sumCoins+=costs[i];
+            if(sumCosts+costs[i]>coins) break;
+            sumCosts+=costs[i];
             result+=1;
         }
 
